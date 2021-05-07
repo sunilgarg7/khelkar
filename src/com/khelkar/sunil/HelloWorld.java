13b0ed3437594f32d31d4ad0fa2aa5a54acff0ee
Sunil Garg <snl.garg@yahoo.com>
2021-05-07 20:29:40 +0530

    Hello World
    Ram Ram bhaaya!!

---
+++ src/com/khelkar/sunil/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.khelkar.sunil;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// Mandatory "Hello world" program to set Shagun ;-)
+		System.out.println("Hello World");
+		System.out.println("Sabko Ram Ram !!");
+	}
+}
