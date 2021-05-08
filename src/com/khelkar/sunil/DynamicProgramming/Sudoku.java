cc74e1a34feffce35274b5ca4149107eaef9ddc3
Sunil Garg <snl.garg@yahoo.com>
2021-05-08 10:21:50 +0530

    Added more comments for sudoku

--- src/com/khelkar/sunil/DynamicProgramming/Sudoku.java
+++ src/com/khelkar/sunil/DynamicProgramming/Sudoku.java
@@ -7,12 +7,15 @@ public class Sudoku {
 	public static void main(String[] args) {
 		
 		int[][] board = new int[9][9];
-		// randomly filled sudoku board or we should get board as input to our program
-//		display(board);
+		// randomly filled sudoku board or we should get board as input to our program,
+		// the logic below will take care of pre-filled enteries and will print all the possible valid sudoku.
+		// if this program is run without any values filled in board then it will print all valid sudokus that possible in this universe for 9 * 9 Board offcourse which is hard coded.
+		// display(board);
 		
-		boolean[][] rowarr = new boolean[board.length][board.length + 1]; // +1 because my values lies from 1 to 9 instead of 0 to 8
+		boolean[][] rowarr = new boolean[board.length][board.length + 1]; // +1 because my values lies from 1 to 9 instead of 0 to 8 & we dont want to do a -1 everytime while putting value in this
 		boolean[][] colsArr = new boolean[board.length][board.length + 1];
 		HashMap<String, boolean[]> subgroup = new HashMap<String, boolean[]>();
+		// creating with hand, because I am too lazy to code for this small thing ;-)
 		subgroup.put("00", new boolean[board.length + 1]);
 		subgroup.put("03", new boolean[board.length + 1]);
 		subgroup.put("06", new boolean[board.length + 1]);
@@ -23,8 +26,8 @@ public class Sudoku {
 		subgroup.put("63", new boolean[board.length + 1]);
 		subgroup.put("66", new boolean[board.length + 1]);
 		
-		// we should traverse our given sudoku board to 
-		
+		// we should traverse our given sudoku board once so that we can put those value inside our rowarr, colsArr and subgroup in-memory maps, to reduce 
+		// the number of invalid cases in those given input.
 		sudoku(board, 0, 0, rowarr, colsArr, subgroup);
 		
 	}
@@ -58,35 +61,37 @@ public class Sudoku {
 			sudoku(board, nI, nJ, rowArr, colsArr, subgroup);
 			return;
 		} else  {
+			// Question that were coming to my mind while solving or these should come to your mind while solving.
+			// How we are going to say that this board cannot proceed further as we have
+			// selected some of the vals at particular place which are not apt, so how do we get back
+			// to the original space ?? this will help you understand what all parameters are required
+		        // while doing backtracking or recursive calls;
 		
 		for (int val = 1; val <= 9; val++) {
 			// if any of the rowArr or colsArr or subGroup contains this val for given i & j then we also have to do nothing 
 			
 		  if (rowArr[i][val] == false && colsArr[j][val] == false && subgroup.get("" + subgroupI + subGroupj)[val] == false) {
-				rowArr[i][val] 										= true;
-				colsArr[j][val] 									= true;
-				subgroup.get("" + subgroupI + subGroupj)[val]		= true;
-				board[i][j] 										= val;
+				rowArr[i][val] 							= true;
+				colsArr[j][val] 						= true;
+				subgroup.get("" + subgroupI + subGroupj)[val]			= true;
+				board[i][j] 							= val;
 				sudoku(board, nI, nJ, rowArr, colsArr, subgroup);
 				// clear whatever value we have set in this iteration.
-				board[i][j] 										= 0;
-				rowArr[i][val] 										= false;
-				colsArr[j][val] 									= false;
-				subgroup.get("" + subgroupI + subGroupj)[val]		= false;
+				board[i][j] 							= 0;
+				rowArr[i][val] 							= false;
+				colsArr[j][val] 						= false;
+				subgroup.get("" + subgroupI + subGroupj)[val]			= false;
 			}
-					// How we are going to say that this board cannot proceed further as we have
-					// selected some of the vals at particular place which are not apt, so how do we get back
-				    // to the original space ?? this will help you understand what all parameters are required
-					// while doing backtracking or recursive calls;
+			
 		} // End Of value loop 1 to 9 
-		}	
+	    }	
 	}
 	
 	public static void display(int[][] board) {
 		String str = "";
 		for (int i=0; i < board.length; i++) {
 			for (int j=0; j< board.length; j++) {
-				str +=  board[i][j] + " ";
+				str +=  board[i][j] + " "; // we could use String.format, but hey I am lazy ok?
 			}
 			str += "\n";
 		}
