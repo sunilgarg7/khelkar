9df56ee37e68be2e23dc687b3602c490172fd560
Sunil Garg <snl.garg@yahoo.com>
2021-05-17 18:46:33 +0530

    printing frequency of the characters, without printing the duplicate characters again and again

---
+++ src/com/khelkar/sunil/string/FrequencyPrintingInStringCharacterAppearnceOrderWithoutDuplicates.java
@@ -0,0 +1,23 @@
+package com.khelkar.sunil.string;
+
+public class FrequencyPrintingInStringCharacterAppearnceOrderWithoutDuplicates {
+	public static void main(String[] args) {
+
+		// printing frequency of the characters, without printing the duplicate
+		// characters again and agains
+		String str = "acbdhsfoosdhfohoas";
+
+		int[] freq = new int[255];
+
+		for (int i = 0; i < str.length(); i++) {
+			freq[str.charAt(i)]++;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			if (freq[str.charAt(i)] != 0) {
+				System.out.println("freq of char [ " + str.charAt(i) + " ] = " + freq[str.charAt(i)]);
+				freq[str.charAt(i)] = 0;
+			}
+		}
+	}
+}
