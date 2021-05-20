8501eb98cfffce3b0de1bd2316cb6e6e7506e568
Sunil Garg <snl.garg@yahoo.com>
2021-05-20 18:21:27 +0530

    Added code to understand the effect on iterations

--- src/com/khelkar/sunil/DynamicProgramming/ShoppingAmazon.java
+++ src/com/khelkar/sunil/DynamicProgramming/ShoppingAmazon.java
@@ -3,17 +3,20 @@ package com.khelkar.sunil.DynamicProgramming;
 import java.util.Arrays;
 
 public class ShoppingAmazon {
+	public static int iterations = 0;
 
 	// Very Important question to understand, how we can reduce the total number of iteration when brute force is the only solution
 	// With Array remember your two weapon, either Sort them or use the in-place replace
 	public static void main(String[] args) {
 		
+		
+		
 		int budget = 1000;
 		
-		int[] jeans= {500,30};
-		int[] shoes= {100,150,200};
-		int [] skirt= {300, 400, 500};
-		int[] top = {78,989,234};
+		int[] jeans= {500,30, 555,150,200, 666,432,234};
+		int[] shoes= {100,150,200, 666,432,234};
+		int [] skirt= {300, 400, 500,232,212,236,786};
+		int[] top = {78,989,234,23,43,12,150,200, 666,432,234};
 		
 		// sort these arrays before passing to function
 		Arrays.sort(jeans);
@@ -27,10 +30,14 @@ public class ShoppingAmazon {
 		
 		int res =shop(jeans, shoes, skirt, top, budget, 1);
 		System.out.println(res);
+		
+		System.out.println("Iterations this algo took:  " + iterations);
 	}
 	
 	
 	public static int shop(int[] jeans, int[] shoes, int [] skirt, int[] top, int budget,int type) {
+		
+		iterations++;
 		
 		if (budget < 0) {
 			return 0;
@@ -71,7 +78,7 @@ public class ShoppingAmazon {
 				// the other rest elements as we have already sorted array.
 				// as per above comments no further element can give us any more permutation. so short-circuit here ;-)
 				// this reduces our overall run-time
-				return count;
+				return count; // want to see the difference in number of iteration, just comment this line and note the number of iteration number getting increased
 			}
 			
 			count += temp;
